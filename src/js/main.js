972ad0272b15199d1dce54dddb05af88217d1b51
Dxrry <17220455@bsi.ac.id>
2024-04-21 15:42:21 -0400

    Removed jQuery to ensure compatibility with Tampermonkey. Optimized captcha solving logic for improved performance.

--- src/js/main.js
+++ src/js/main.js
@@ -1,41 +1,37 @@
+// ==UserScript==
+// @name         MyBEST: Captcha Solver
+// @namespace    http://tampermonkey.net/
+// @version      1.1
+// @description  Save time by using this extension to solve captchas for you on elearning.bsi.ac.id/login.
+// @author       Dxrry
+// @match        *://elearning.bsi.ac.id/login
+// @grant        none
+// ==/UserScript==
+
+// You can paste this on Tampermonkey
+
 (function () {
-    const getCaptcha = $("#captcha_question");
-    if($("#captcha_question").length > 0) {
-        const getCaptchaText = getCaptcha.text();
-        const regexPattern = /(\d+)\s*([-+*/])\s*(\d+)/;
-        const isMatch = getCaptchaText.match(regexPattern);
+    const getCaptcha = document.getElementById("captcha_question");
 
-        if(isMatch) {
-            const firstNumber = parseInt(isMatch[1]);
-            const secondNumber = parseInt(isMatch[3]);
+    if (!getCaptcha) return;
 
-            // in the future, maybe not just using +
-            const operator = isMatch[2];
-            
-            let mathResult;
-            switch(operator) {
-                case '+':
-                    mathResult = (firstNumber + secondNumber);
-                    break;
-                case '-':
-                    mathResult = (firstNumber - secondNumber);
-                    break;
-                case '*': // case 'x':
-                    mathResult = (firstNumber * secondNumber);
-                    break;
-                case '/':
-                    mathResult = (firstNumber / secondNumber);
-                    break;
-                default:
-                    console.log(
-                        {
-                            "app": "MyBEST: Captcha Solver",
-                            "error": "INVALID OPERATOR"
-                        }
-                    );
-                    return;
-            }
-            $("#captcha_answer").val(mathResult);
-        }
-    }
+    const match = getCaptcha.textContent.match(/(\d+)\s*([-+*/])\s*(\d+)/);
+
+    if (!match) return;
+
+    const [_, firstNumber, operator, secondNumber] = match.map(
+        (value, key) => (key === 1 || key === 3) ? parseInt(value, 10) : value
+    );
+    
+    const operators = {
+        '+': (a, b) => a + b,
+        '-': (a, b) => a - b,
+        'x': (a, b) => a * b,
+        '/': (a, b) => a / b
+    }; // Just prepare for the future
+
+    const mathResult = operators[operator](firstNumber, secondNumber);
+    const captchaAnswer = document.getElementById("captcha_answer");
+
+    if (captchaAnswer) captchaAnswer.value = mathResult;
 })();
